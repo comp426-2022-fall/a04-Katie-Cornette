cf2d0f3afff8f58a0cf1d19b212282c2bd4541f8
Katie Cornette <kathcorn@email.unc.edu>
2022-11-20 23:18:35 -0500

    slowly adding things

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
-import minimist from "minimist"
+import minimist from "minimist" 
 import express from "express"
 import { roll } from "./lib/roll.js" 
+
+const app = express();
+const args = minimist(process.argv.slice(2)); 
+
+vars port = args.port ? args.port: 5000
+
+
