c64ad9fc2155504581d57e086d17dbcd6670db76
Katie Cornette <kathcorn@email.unc.edu>
2022-11-21 00:39:56 -0500

    slowly adding things 3

--- server.js
+++ server.js
@@ -8,8 +8,14 @@ const args = minimist(process.argv.slice(2));
 
 const port = args.port ? args.port: 5000
 
+//checking endpoint
 app.get('/app/, (req, res) => {
 	res.send('200 OK');
 }); 
+
+//endpoint app roll
+app.get('/app/roll/, (req, res) => {
+	res.send(roll(6,2,1));
+}
 
 
